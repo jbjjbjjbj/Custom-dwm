be7a88088cf92f4739d83e7b7c1eb8c9a3b99aab
Julian <j@teule.nl>
2018-09-22 21:29:40 +0200

    Qutebrowser goes to tag 9

--- config.h
+++ config.h
@@ -33,6 +33,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Thunderbird",  NULL,       NULL,       1 << 7,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "qutebrowser", NULL, NULL,          1 << 8,      0 ,           -1 },
 };
 
 /* layout(s) */
